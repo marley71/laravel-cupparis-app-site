7a525169112b6f3bb0954b27e73ae536c4c99e77
vagrant <vagrant@homestead>
2020-12-21 14:07:03 +0000

    add web route

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,22 +1,22 @@
 <?php
-
-Route::group([
-    'namespace' => "App\\Http\\Controllers",
-], function () {
-    $prefix = config('cupparis-site.route_prefix');
-    Route::get("$prefix", 'CupSiteController@page')->name('cup_site');
-    Route::get("$prefix/{menu?}", 'CupSiteController@page')->name('cup_site');
-    Route::get("$prefix/news/{menu?}", 'CupSiteController@news')->name('cup_site_news');
-    Route::get("$prefix/eventi/{menu?}", 'CupSiteController@news')->name('cup_site_eventi');
-    //Route::get("cup-site-admin", 'CupSiteController@admin')->name('cup_site_admin');
-});
-
-
-Route::group([
-    'namespace' => "App\\Http\\Controllers",
-    'middleware' => 'web'
-], function () {
-    //$prefix = config('cupparis-site.route_prefix');
-    //Route::get("$prefix/{menu?}", 'CupSiteController@page')->name('cup_site');
-    Route::get("cup-site-admin", 'CupSiteController@admin')->name('cup_site_admin');
-});
+//
+//Route::group([
+//    'namespace' => "App\\Http\\Controllers",
+//], function () {
+//    $prefix = config('cupparis-site.route_prefix');
+//    Route::get("$prefix", 'CupSiteController@page')->name('cup_site');
+//    Route::get("$prefix/{menu?}", 'CupSiteController@page')->name('cup_site');
+//    Route::get("$prefix/news/{menu?}", 'CupSiteController@news')->name('cup_site_news');
+//    Route::get("$prefix/eventi/{menu?}", 'CupSiteController@news')->name('cup_site_eventi');
+//    //Route::get("cup-site-admin", 'CupSiteController@admin')->name('cup_site_admin');
+//});
+//
+//
+//Route::group([
+//    'namespace' => "App\\Http\\Controllers",
+//    'middleware' => 'web'
+//], function () {
+//    //$prefix = config('cupparis-site.route_prefix');
+//    //Route::get("$prefix/{menu?}", 'CupSiteController@page')->name('cup_site');
+//    Route::get("cup-site-admin", 'CupSiteController@admin')->name('cup_site_admin');
+//});
